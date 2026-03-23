d66212b5c0466fc04e093f5f97b170ee265b15ad
xiaoyuan <fengy266@mail2.sysu.edu.cn>
2026-03-23 20:13:33 +0800

    chore: 清理多余文件（tmp、__pycache__、stitch原型、dist、截图、Beautify）

--- desktop/Beautify/control_content_widgets/__init__.py
+++
@@ -1,9 +0,0 @@
-from .glass_frame import GlassFrame
-from .vertical_gauge import BatteryGaugeWidget, SpeedGaugeWidget, VerticalGaugeWidget
-
-__all__ = [
-    "BatteryGaugeWidget",
-    "GlassFrame",
-    "SpeedGaugeWidget",
-    "VerticalGaugeWidget",
-]
